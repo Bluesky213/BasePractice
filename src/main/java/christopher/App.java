711f9be9bf8de669fb5a6287b5236985a252a981
Christopher Chen <chenqin.bluesky@gamil.com>
2020-07-26 01:27:21 +0800

    commit

--- src/main/java/christopher/App.java
+++ src/main/java/christopher/App.java
@@ -1,9 +1,7 @@
 package christopher;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * Hello world!
