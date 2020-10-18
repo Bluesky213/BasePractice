f13ad1fd812d036d485114531ebc9a9f94c78490
Christopher Chen <chenqin.bluesky@gamil.com>
2020-10-18 17:23:08 +0800

    MQ消费者,Aliyun2带宽1Mbps，消费128Kb/s，"Hello World!xxxx"，8byte 平均消费速度 1300条/s

---
+++ src/main/java/christopher/practice1/RabbitMQConsumerExample.java
@@ -0,0 +1,43 @@
+package christopher.practice1;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class RabbitMQConsumerExample {
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("8.129.42.155");
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare("queue", false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+        //回调消费消息
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                    throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+                try {
+                    doWork(message);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    System.out.println(" [x] Done");
+                }
+            }
+        };
+        channel.basicConsume("queue", true, consumer);
+    }
+
+    private static void doWork(String task) throws InterruptedException {
+        for (char ch: task.toCharArray()) {
+
+            if (ch == '.') {
+                Thread.sleep(1000);
+            }
+        }
+    }
+}
