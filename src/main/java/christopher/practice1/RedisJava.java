5753ac367d476b298bf373b2a42b9a7dcac227a8
Christopher Chen <chenqin.bluesky@gamil.com>
2020-12-28 08:35:52 +0800

    Redis Conn

---
+++ src/main/java/christopher/practice1/RedisJava.java
@@ -0,0 +1,16 @@
+package christopher.practice1;
+
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import redis.clients.jedis.Jedis;
+
+public class RedisJava {
+    public static void main(String[] args) {
+        //连接本地的 Redis 服务
+        Jedis jedis = new Jedis("8.129.42.155",6379);
+        // 如果 Redis 服务设置来密码，需要下面这行，没有就不需要
+        // jedis.auth("123456"); 
+        System.out.println("连接成功");
+        //查看服务是否运行
+        System.out.println("服务正在运行: "+jedis.ping());
+    }
+}
